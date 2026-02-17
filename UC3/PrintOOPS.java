86dea2ce55cd6f4f6f9f1d62a14afbb51c1ff845
shiyamyete <shiyamyete@gmail.com>
2026-02-17 23:02:14 +0530

    Codes UC1,2,3 to Print OOPS in all three ways

---
+++ UC3/PrintOOPS.java
@@ -0,0 +1,11 @@
+public class PrintOOPS {
+    public static void main(String[] args) {
+        System.out.println(String.join("   ", "  *** ", "  *** ", " ***** ", " ***** "));
+        System.out.println(String.join("   ", " ** **", " ** **", " ** **", " ** "));
+        System.out.println(String.join("   ", "** **", "** **", "** ** ", "** "));
+        System.out.println(String.join("   ", "** **", "** **", "***** ", " ***** "));
+        System.out.println(String.join("   ", "** **", "** **", "** ", "     **"));
+        System.out.println(String.join("   ", " ** **", " ** **", "** ", "     **"));
+        System.out.println(String.join("   ", "  *** ", "  *** ", "** ", " ***** "));
+    }
+}
